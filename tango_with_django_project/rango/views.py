fb5128d3354d929eefea82328cbc45f5983d75be
Nadia McLaren <2648028m@student.gla.ac.uk>
2024-02-08 20:02:15 +0000

    Chapter 9 completed

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -8,6 +8,8 @@ from rango.forms import PageForm
 from django.shortcuts import redirect
 from django.urls import reverse
 from rango.forms import UserForm, UserProfileForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 
 def index(request):
@@ -41,6 +43,7 @@ def show_category(request, category_name_slug):
 
     return render(request, 'rango/category.html', context=context_dict)
 
+@login_required
 def add_category(request):
     form = CategoryForm()
 
@@ -53,6 +56,7 @@ def add_category(request):
             print(form.errors)
     return render(request, 'rango/add_category.html', {'form': form})
 
+@login_required
 def add_page(request, category_name_slug):
     try:
         category = Category.objects.get(slug=category_name_slug)
@@ -129,3 +133,14 @@ def user_login(request):
 
     else:
         return render(request, 'rango/login.html')
+
+@login_required
+def restricted(request):
+    context_dict={}
+    context_dict[0] = "Since you're logged in, you can see this text!"
+    return render(request, 'rango/restricted.html', context_dict)  
+
+@login_required
+def user_logout (request):
+    logout(request)
+    return redirect(reverse('rango:index'))
